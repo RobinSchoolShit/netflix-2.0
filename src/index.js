89dfc2fc80b2e87d646868137f68256a5c47b414
RobinSchoolShit <200512@student.glu.nl>
2022-03-18 19:07:15 +0100

    Global styles styled components gedaan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
-import GlobalStyles from './global-styles'
+import { GlobalStyles } from './global-styles'
 
 render(<><GlobalStyles/><App /></>, document.getElementById('root'));
