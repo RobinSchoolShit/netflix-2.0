686d639738f45dee38320736333fccfc1876d5a9
RobinSchoolShit <200512@student.glu.nl>
2022-04-07 09:46:47 +0200

    React router klaar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Routes, Route } from 'react-router-dom'
-
+import { Home, Browse, Signin, Signup } from './pages'
 import * as ROUTES from './constants/routes'
 
 export default function App() {
   return (
     <Routes>
-      <Route path={ROUTES.HOME}>
-
+      <Route exact path={ROUTES.HOME}>
+        <Home/>
       </Route>
-      <Route path='/user'>
-        <h1>Dikke vette huts</h1>
+      <Route exact path='/signup'>
+        <Signup/>
+      </Route>
+      <Route exact path='/signin'>
+        <Signin/>
+      </Route>
+      <Route exact path='/browse'>
+        <Browse/>
       </Route>
     </Routes>
   )
