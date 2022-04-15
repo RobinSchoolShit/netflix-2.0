3d463a3cc526534eeff1fdabd0ac07c931530197
RobinSchoolShit <200512@student.glu.nl>
2022-04-15 22:34:32 +0200

    Netflix 2.0 met React helemaal klaar

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react'
 import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import { Home, Browse, Signin, Signup } from './pages'
 import * as ROUTES from './constants/routes'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
+
+  const { user } = useAuthListener();
+
   return (
     // De code hieronder is zodat alle pages de goede URL hebben doormiddel van React Router
     <Routes>
-      <Route exact path={ROUTES.HOME}>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact path={ROUTES.HOME}>
         <Home/>
-      </Route>
-      <Route exact path='/signup'>
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact path={ROUTES.SIGN_UP}>
         <Signup/>
-      </Route>
-      <Route exact path='/signin'>
+        </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact path={ROUTES.SIGN_IN}>
         <Signin/>
-      </Route>
-      <Route exact path='/browse'>
+        </IsUserRedirect>
+        <ProtectedRoute user={user} exact path={ROUTES.BROWSE}>
         <Browse/>
-      </Route>
+      </ProtectedRoute>
     </Routes>
   )
 }
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
 export default function Browse() {
-    // De browse page waar je naar toegaat na het inloggen of registreren
-    return <p>Hello from the browse</p>
+    // Dit is een variabele voor de series en films uit de database van Firebase
+    const { series } = useContent('series');
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
+
+  return <BrowseContainer slides={slides} />;
 }
